d7ec8425b52a551d8e71e9aa9d55aad20fe0f956
vse-ok-ninok <ninka867@yandex.ru>
2025-08-18 23:02:38 +0300

    3-dz - исправленное

--- 2-dice/app02.js
+++ 2-dice/app02.js
@@ -2,7 +2,7 @@
 
 function diceRandom(typeDice) {
     const max = Number(typeDice.slice(1));
-    return Math.floor(Math.random() * (max - 1) + 1);
+    return Math.floor(Math.random() * max  + 1);
 };
 
-console.log(diceRandom('d7'));
+console.log(diceRandom('d8'));
